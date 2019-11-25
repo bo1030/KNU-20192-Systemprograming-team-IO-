4b6d16a52a3553bcd269a6fe38de4cd779d35a17
bochan <qhcks1030@naver.com>
2019-11-25 06:36:09 +0000

    add gui.c and ls.c

---
+++ src/gui.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void underdock();
+void menu();
+void center_left(char *);
+void center_right(char *);
+void ls(char *);
+
+int main(void)
+{
+	char list[][];	
+	ls(
+}
+
+void underdock(char * under)
+{
+
+}
+
+void menu(char* menu)
+{
+}
+
+void center_left(char *list)
+{
+}
+
+void center_left(char *list)
+{
+}
