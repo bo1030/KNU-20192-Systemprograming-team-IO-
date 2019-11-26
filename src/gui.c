6162c319f35cbfc6443f5e536314433e0e01b3b8
Bochan kim <44803830+bo1030@users.noreply.github.com>
2019-11-26 22:51:31 +0900

    merge ls to gui

--- src/gui.c
+++ src/gui.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 void underdock();
 void menu();
 void center_left(char *);
 void center_right(char *);
 void ls(char *);
+void do_ls(char[]);
+void dostat(char *);
+void show_file_info(char *, struct stat *);
 
 int main(void)
 {
@@ -28,4 +35,37 @@ void center_left(char *list)
 
 void center_left(char *list)
 {
+}
+
+void do_ls(char dirname[])
+{
+	DIR *dir_ptr;
+	struct dirent *direntp;
+	if ((dir_ptr = opendir(dirname)) == NULL)
+		fprintf(stderr, "ls2: cannot open %s\n", dirname);
+	else
+	{
+		while ((direntp = readdir(dir_ptr)) != NULL)
+			dostat(direntp->d_name);
+		closedir(dir_ptr);
+	}
+}
+
+void dostat(char *filename)
+{
+	struct stat info;
+
+	if (stat(filename, &info) == -1)
+		perror(filename);
+	else
+		show_file_info(filename, &info);
+}
+
+void show_file_info(char *filename, struct stat *info_p)
+{
+	char *ctime();
+
+	printf("%8ld ", (long)info_p->st_size);
+	printf("%.12s ", 4 + ctime(&info_p->st_mtime));
+	printf("%s\n", filename);
 }
